22aacf9875bd9f7719112e2c6f30a412334e189e
AymanAkashi <aaggoujj@student.1337.ma>
2023-12-21 11:36:52 +0100

    feat: solve Gary Code

--- cses/intorductory/Gray_Code.cpp
+++ cses/intorductory/Gray_Code.cpp
@@ -1,61 +1,18 @@
 #include <iostream>
-#include <vector>
-#include <cstring>
-#include <cmath>
-
+#include <bitset>
 
 using namespace std;
 
-string grayCode(int n, string &empty){
-	if (n == 0)
-	{
-		empty = "0";
-		return "0";
-	}
-	if (n == 1)
-	{
-		empty = "1";
-		return "1";
-	}
-	if (n == 2)
-	{
-		empty = "10";
-		return "10";
-	}
-	return grayCode(n/2, empty) + grayCode(n%2 == 0 ? 0 : 1, empty);
-}
-
-string empty(int n, int len){
-	string ans = "";
-	for (int i = 0; i < len - n; ++i)
-	{
-		ans += "0";
-	}
-	return ans;
-
-}
 
 int main(){
-	int n;
-	cin >> n;
-	vector<string> ans;
-	int x = pow(2, n);
-	string last = "";
-	for (int i = 0; i < x; ++i)
-	{
-		ans.push_back(grayCode(i, last));
-	}
-	for (int i = 0; i < ans.size(); ++i)
-	{
-		if (i % n == 0)
-			cout << empty(ans[i].size(), ans.back().size()) + ans[i] << endl;
-	}
+	int n; cin >> n;
 
-	for (int i = ans.size() - 1; i >= 0; --i)
-	{
-		if (i % n != 0)
-			cout << empty(ans[i].size(), ans.back().size()) + ans[i] << endl;
+	for(int i = 0; i < (1 << n); i++){
+		int val = (i ^ (i >> 1));
+		bitset<16> r(val);
+		string s = r.to_string();
+		cout << s.substr(16 - n) << endl;
 	}
-
-	return 0;
 }
+
+// url: https://cses.fi/problemset/result/7986602/
